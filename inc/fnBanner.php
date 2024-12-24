27edd46b2ba5e2228af989002c1f19b456dd5590
Matheus Alves <54443770+matthaiosalves@users.noreply.github.com>
2024-12-24 18:57:13 -0300

    fix: Ajustado o caminho relativo

--- inc/fnBanner.php
+++ inc/fnBanner.php
@@ -1,9 +1,10 @@
 <?php
 function get_custom_banner_url()
 {
+  $site_url = home_url($_SERVER['REQUEST_URI']); // Captura o caminho completo
   $banner_url = has_post_thumbnail()
     ? get_the_post_thumbnail_url(null, 'full')
-    : esc_url(get_site_url() . '/wp-content/themes/cadfiber/img/imagem-aperto-de-maos-sem-mascara.webp');
+    : esc_url($site_url . '/wp-content/themes/cadfiber/img/imagem-aperto-de-maos-sem-mascara.webp');
 
   return esc_url($banner_url);
 }
