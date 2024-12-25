da224b5bd73f088fe3e7c2e8131f37b471d7879c
Matheus Alves <54443770+matthaiosalves@users.noreply.github.com>
2024-12-25 09:41:37 -0300

    fix: Ajuste no nome do tema

--- inc/fnBanner.php
+++ inc/fnBanner.php
@@ -2,7 +2,7 @@
 function get_custom_banner_url()
 {
   $home_url = get_home_url();
-  $theme_path = '/wp-content/themes/cadfiber/img/imagem-aperto-de-maos-sem-mascara.webp';
+  $theme_path = '/wp-content/themes/cadfiber-bundle/img/imagem-aperto-de-maos-sem-mascara.webp';
   $clean_url = preg_replace('#(/[^/]+)(?=/\1)#', '', $home_url);
   $banner_url = has_post_thumbnail()
     ? get_the_post_thumbnail_url(null, 'full')
