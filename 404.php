8e154bf260b117b133ae5e181c60828573f63ef4
Matheus Alves <54443770+matthaiosalves@users.noreply.github.com>
2024-12-31 19:42:33 -0300

    fix: Ajuste na 404

--- 404.php
+++ 404.php
@@ -18,10 +18,10 @@ get_header();
       </div>
     </div>
   </div>
+  <div class="boxImagem">
+    <img loading="lazy" src="<?php echo get_template_directory_uri(); ?>/img/imagem-aperto-de-maos-sem-mascara.webp" alt="" class="imagem">
+  </div>
 </section>
-<div class="boxImagem">
-  <img loading="lazy" src="<?php echo get_template_directory_uri(); ?>/img/imagem-aperto-de-maos-sem-mascara.webp" alt="" class="imagem">
-</div>
 <div class="boxTreco">
   <img loading="lazy" src="<?php echo get_template_directory_uri(); ?>/img/treco-verde-internas.svg" alt="">
 </div>
